b40f4bc22c1b590068249be2acb604f47b9eb802
ibrahim lukman <ibrahimlukman147@gmail.com>
2022-09-09 05:15:30 -0700

    Task done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,10 @@
-
 #include <stdio.h>
-
 /**
-* main - print the string in the put function
-* 
-* Description: using the main function
-* this program prints "programming is like building a multilingual puzzle
-* Return: 0
-*/
+ * main - entry point
+ * Return: 0
+ * */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-^C
-^c
