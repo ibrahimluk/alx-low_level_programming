c292ebc7c956dfccf90237debfd857ec80ea3d9a
ibrahim lukman <ibrahimlukman147@gmail.com>
2022-09-16 08:57:18 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 numbers
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (i > 9)
+		{
+			_putchar(i / 10 + '0');
+		}
+		_putchar(i % 10 + '0');
+	}
+	_putchar('\n');
+}
