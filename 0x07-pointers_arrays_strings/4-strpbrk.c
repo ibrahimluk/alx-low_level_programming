95f0f0e2c46ec6f8fb4cf6a5ce07870cc3d1bf52
ibrahim lukman <ibrahimlukman147@gmail.com>
2022-09-26 14:42:15 -0700

    Write a function that searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,6 +4,7 @@
  * _strpbrk - locates first occurence of any bytes from accept in s
  *
  * @s: string to work on
+ * @accept: pattern to match against
  * Return: pointer to the first match
  */
 char *_strpbrk(char *s, char *accept)
