d9972d5bc386cb02af8b50b3940015bc329521aa
ibrahim lukman <ibrahimlukman147@gmail.com>
2022-10-12 15:35:00 -0700

    task done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i, (*a)(int, char **) = main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	printf("%.2x", *(unsigned char *)a);
+	a++;
+	for (i = 1; i < atoi(argv[1]); i++)
+	{
+		printf(" %.2x", *(unsigned char *)a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
