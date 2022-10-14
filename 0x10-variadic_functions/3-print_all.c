5965e927b404c7832d34fed5861370fbf6736c61
ibrahim lukman <ibrahimlukman147@gmail.com>
2022-10-14 14:30:44 -0700

    task done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,113 +1,97 @@
-#include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
+#include"variadic_functions.h"
 
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_all(const char * const format, ...);
+
 
 /**
- * print_char - Prints a char.
- * @arg: A list of arguments pointing to
- * the character to be printed.
- */
-void print_char(va_list arg)
+*
+**print_all-printsall
+*
+**@format:param
+*
+**/
+
+voidprint_all(constchar*constformat,...)
+
 {
-	char letter;
 
-	letter = va_arg(arg, int);
-	printf("%c", letter);
-}
+		va_listvalist;
 
-/**
- * print_int - Prints an int.
- *
- *  @arg: A list of arguments pointing to
- *  the integer to be printed.
- */
-void print_int(va_list arg)
-{
-	int num;
+			unsignedinti=0,j,k=0;
 
-	num = va_arg(arg, int);
-	printf("%d", num);
-}
+				char*str;
 
-/**
- * print_float - Prints a float.
- * @arg: A list of arguments pointing to
- * the float to be printed.
- */
-void print_float(va_list arg)
-{
-	float num;
-
-	num = va_arg(arg, double);
-	printf("%f", num);
-}
-
-/**
- * print_string - Prints a string.
- * @arg: A list of arguments pointing to
- * the string to be printed.
- */
-void print_string(va_list arg)
-{
-	char *str;
-
-	str = va_arg(arg, char *);
-
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-
-	printf("%s", str);
-}
-
-/**
- * print_all - Prints anything, followed by a new line.
- * @format: A string of characters representing the argument types.
- * @...: A variable number of arguments to be printed.
- * Description: Any argument not of type char, int, float,
- * or char * is ignored.
- * If a string argument is NULL, (nil) is printed instead.
- */
-oid print_all(const char * const format, ...)
-{
-	va_list args;
-	int i = 0, j = 0;
-	char *separator = "";
-	printer_t funcs[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string}
-	};
-
-	va_start(args, format);
-
-	while (format && (*(format + i)))
-	{
-		j = 0;
+					constchart_arg[]="cifs";
 
 
-		j++;
 
-		if (j < 4)
-		{
-			printf("%s", separator);
-			funcs[j].print(args);
-			separator = ", ";
-		}
+						va_start(valist,format);
 
-		i++;
-	}
+							while(format&&format[i])
 
-	printf("\n");
+									{
 
-	va_end(args);
+												j=0;
+
+														while(t_arg[j])
+
+																	{
+
+																					if(format[i]==t_arg[j]&&k)
+
+																									{
+
+																														printf(",");
+
+																																		break;
+
+																																					}j++;
+
+																							}
+
+																switch(format[i])
+
+																			{
+
+																						case'c':
+
+																										printf("%c",va_arg(valist,int)),k=1;
+
+																													break;
+
+																															case'i':
+
+																																printf("%d",va_arg(valist,int)),k=1;
+
+																																			break;
+
+																																					case'f':
+
+																																						printf("%f",va_arg(valist,double)),k=1;
+
+																																									break;
+
+																																											case's':
+
+																																												str=va_arg(valist,char*),k=1;
+
+																																															if(!str)
+
+																																																			{
+
+																																																								printf("(nil)");
+
+																																																												break;
+
+																																																															}
+
+																																																		printf("%s",str);
+
+																																																					break;
+
+																																																							}i++;
+
+																	}
+
+								printf("\n"),va_end(valist);
+
 }
