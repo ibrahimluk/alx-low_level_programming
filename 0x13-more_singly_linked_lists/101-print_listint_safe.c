1acd6ec72aab8a9e207ac1b307ce47b7509be424
ibrahim lukman <ibrahimlukman147@gmail.com>
2022-10-25 11:29:25 -0700

    Write a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,34 +1,36 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint_safe - print list
- * @head: head node
- * Return: amount of nodes in list
+ * print_listint_safe - Print a `listint_t` linked list including mem addresses
+ * @head: head of linked list
+ * Description: Go through the list only once.
+ * Return: number of nodes in list. If fails, exit with status 98.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int size = 0, i;
-	const listint_t *tmp[100];
+	const listint_t *current;
+	size_t count;
+	const listint_t *hold;
 
-	if (!head)
+	current = head;
+	if (current == NULL)
 		exit(98);
 
-	while (head)
+	count = 0;
+	while (current != NULL)
 	{
-		for (i = 0; i < size; i++)
+		hold = current;
+		current = current->next;
+		count++;
+		printf("[%p] %d\n", (void *)hold, hold->n);
+
+		if (hold < current)
 		{
-			f (tmp[i] == head)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				return (size);
-			}
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			break;
 		}
-		printf("[%p] %d\n", (void *)head, head->n);
-		tmp[size] = head;
-		size++;
-		head = head->next;
 	}
-	return (size);
+
+	return (count);
 }
